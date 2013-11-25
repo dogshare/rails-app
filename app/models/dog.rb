9d05a02e36ae2573fd616586de76b05328a4e1d5
sonejah21 <sonejah21@hotmail.com>
2013-11-25 18:17:41 -0500

    fixed views for column changes

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,6 +1,7 @@
 class Dog < ActiveRecord::Base
   belongs_to :owner, :class_name => "Person"
-  has_many :dogshares
   has_many :sitters, :through => :dogshares, :class_name => "Person"
-  
+
+  has_many :dogshares
+
 end
--- db/migrate/20131125191918_create_dogshares.rb
+++ db/migrate/20131125191918_create_dogshares.rb
@@ -2,7 +2,7 @@ class CreateDogshares < ActiveRecord::Migration
   def change
     create_table :dogshares do |t|
       t.integer :dog_id
-      t.integer :sitter_id
+      t.integer :sitter_id, :default => nil
       t.boolean :available
 
       t.timestamps
