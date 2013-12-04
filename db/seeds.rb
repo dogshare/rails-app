5cecf62aaeb47a76b407c6b9aa78a2168155c80a
sonejah21 <sonejah21@hotmail.com>
2013-12-04 11:20:26 -0500

    seed data images and tweaking

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,35 +6,115 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+
 # Creating owners
-charlotte = Person.create(:name => "Charlotte", :zip => 11222, :img => File.new("#{Rails.root}/db/demodata/profilecc.jpg"))
-sonja = Person.create(:name => "Sonja", :zip => 22306)
-evan = Person.create(:name => "Evan", :zip => 22306)
-ken = Person.create(:name => "Ken", :zip => 11221)
 
-# Creating dogs and associating owners
-
-rocky = Dog.create(:owner => charlotte, :name => "Rocky", :age => 3, :breed => "Puggle", :description => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", :temperament => "adorable, crazy, stubborn", :active => "very active", :housetrained => true, :picture => true, :special_needs => "none", :dog_snippet => "athletic little monster!", :img => File.new("#{Rails.root}/db/demodata/profilerocky.jpg")) 
-
-lucy = Dog.create(:owner => sonja, :name => "Lucyhawk", :age => 3, :breed => "Kangal", :description => "independent-minded strong girl", :temperament => "difficult with very submissive dogs, sometimes aggressive", :active => "very active", :housetrained => true, :picture => true, :special_needs => "anxiety medication", :dog_snippet => "I'm a loyal companion.")
-
-pesh = Dog.create(:owner => evan, :name => "Peshmerga", :age => 4, :breed => "Kangal", :description => "huge, happy-go-lucky, protective dog", :temperament => "goofy and disobedient", :active => "somewhat active", :housetrained => true, :picture => true, :special_needs => "vegetarian; poultry allergies", :dog_snippet => "I love walks and meeting new people! asjdbasuhd;aishd  ashf;auhsf;iauh  f;iauhf;iauh  jaahsfauhsf")
-
-lester = Dog.create(:owner => sonja, :name => 'Lester', :age => '3', :breed => 'Black and Tan Coonhound', :description => 'super lovable, easy going', :temperament =>'energetic and happy', :active => 'very', :housetrained => true, :picture => true, :special_needs => 'none', :dog_snippet => "I can see for miles!")
-
-adult_supervision = Dog.create(:owner => ken, :name => 'Super', :age => '6', :breed => 'Greyhound', :description => 'aloof, loves napping', :temperament => 'reserved', :active => 'when he wants to be', :housetrained => true, :picture => true, :special_needs => 'none', :dog_snippet => "I run circles around all the cool kids!")
-
+charlotte = Person.create(:name => "Charlotte", 
+  :zip => 11222, 
+  :img => File.new("#{Rails.root}/db/demodata/profilecc.jpg"))
+sonja = Person.create(:name => "Sonja", 
+  :zip => 22306, 
+  :img => File.new("#{Rails.root}/db/demodata/profilesh.jpg"))
+evan = Person.create(:name => "Evan", 
+  :zip => 22306, 
+  :img => File.new("#{Rails.root}/db/demodata/profileej.jpg"))
+ken = Person.create(:name => "Ken", 
+  :zip => 11221, 
+  :img => "")
 
 # Creating sitters
 
-scott = Person.create(:name => "Scott", :zip => 11111, :available_start => '2012-11-14 14:27:46', :available_end => '2012-11-14 14:27:46', :person_snippet => "I love all animals")
-rosie = Person.create(:name => "Rosie", :zip => 22222, :person_snippet => "I have two cats and a dog")
-greg = Person.create(:name => "Greg", :zip => 10003, :person_snippet => "I grew up with 2 dogs")
+scott = Person.create(:name => "Scott", 
+  :zip => 11111, 
+  :available_start => '2012-11-14 14:27:46', 
+  :available_end => '2012-11-14 14:27:46', 
+  :person_snippet => "I love all animals", :img => "")
+rosie = Person.create(:name => "Rosie", 
+  :zip => 22222, 
+  :person_snippet => "I have two cats and a dog", 
+  :img => "")
+greg = Person.create(:name => "Greg", 
+  :zip => 10003, 
+  :person_snippet => "I grew up with 2 dogs", 
+  :img => "")
 
-# Create dogshare
+# Creating dogs and associating owners
 
-rocky.dogshares.build(:owner => charlotte, :sitter => scott)
-rocky.dogshares.build(:owner => charlotte)
-rocky.save
+rocky = Dog.create(:owner => charlotte, 
+  :name => "Rocky", 
+  :age => 3, 
+  :breed => "Puggle", 
+  :description => "I'm cool as a cucumber, love my toys, and enjoy long romps in the park", 
+  :temperament => "adorable, crazy, stubborn", 
+  :active => "very active", 
+  :housetrained => true, 
+  :picture => true, 
+  :special_needs => "none", 
+  :dog_snippet => "athletic little monster!", 
+  :img => File.new("#{Rails.root}/db/demodata/profilerocky.jpg"), 
+  :available_start => "", 
+  :available_end => "") 
+
+lucy = Dog.create(:owner => sonja, 
+  :name => "Lucyhawk", 
+  :age => 3, 
+  :breed => "Kangal", 
+  :description => "I'm an independent-minded strong girl who may or may not like you.", 
+  :temperament => "difficult with very submissive dogs, sometimes aggressive", 
+  :active => "very active", 
+  :housetrained => true, 
+  :picture => true, 
+  :special_needs => "anxiety medication", 
+  :dog_snippet => "I'm a loyal companion.", 
+  :img => File.new("#{Rails.root}/db/demodata/profilelucy.jpg"),
+  :available_start => "", 
+  :available_end => "")
+
+pesh = Dog.create(:owner => evan, 
+  :name => "Peshmerga", 
+  :age => 4, 
+  :breed => "Kangal", 
+  :description => "Yep, I happen to be huge, but I'm also a happy-go-lucky, protective guy who lives in Placitas, NM.",
+  :temperament => "goofy and disobedient", 
+  :active => "somewhat active", 
+  :housetrained => true, 
+  :picture => true, 
+  :special_needs => "vegetarian; poultry allergies", 
+  :dog_snippet => "I love long walks throughout New Mexico.", 
+  :img => File.new("#{Rails.root}/db/demodata/profilepesh.jpg"),
+  :available_start =>"", 
+  :available_end => "")
+
+lester = Dog.create(:owner => sonja, 
+  :name => 'Lester', 
+  :age => '3', 
+  :breed => 'Black and Tan Coonhound', 
+  :description => "Hey friends! I am a super lovable, easy going pup. I love kissing and my hunting instincts will tear your arm off if you're not prepared for squirrel and bird chasing.", 
+  :temperament =>'energetic and happy', 
+  :active => 'very', 
+  :housetrained => true, 
+  :picture => true, 
+  :special_needs => 'none', 
+  :dog_snippet => "I see with my nose.", 
+  :img => File.new("#{Rails.root}/db/demodata/profilelester.jpg"), 
+  :available_start => "", 
+  :available_end => "")
+
+adult_supervision = Dog.create(:owner => ken, 
+  :name => 'Super', 
+  :age => '6', 
+  :breed => 'Greyhound', 
+  :description => 'aloof, loves napping', 
+  :temperament => 'reserved', 
+  :active => 'when he wants to be', 
+  :housetrained => true, 
+  :picture => true, 
+  :special_needs => 'none', 
+  :dog_snippet => "I run circles around all the cool kids!", 
+  :img => "", 
+  :available_start => "", 
+  :available_end => "")
+
+
 
 
