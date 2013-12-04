0a6e36b2cddacb620a51c8f2c836fb8980624e2b
Charlotte Chang <yellowspiderlove@gmail.com>
2013-12-03 22:22:32 -0500

    styling forms

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,5 +4,5 @@ class Person < ActiveRecord::Base
   has_many :sat_dogs, :through => :dogshares, :class_name => "Dogs"
   has_many :dogshares, :foreign_key => "dogshare_id"
 
-  has_attached_file :img, :styles => { :medium => "300x300>", :thumb => "200x200>" }, :default_url => ":style/dog-lover.jpg"
+  has_attached_file :img, :styles => { :medium => "300x300>", :thumb => "200x200>" }, :default_url => ":style/dogpaw.png"
 end
