2797d21e48f1777d19305d1df9dea80a86f015b5
sonejah21 <sonejah21@hotmail.com>
2013-12-05 10:54:59 -0500

    geolocation zip code js

--- app/assets/javascripts/people.js
+++ app/assets/javascripts/people.js
@@ -14,3 +14,27 @@ $(function() {
 });
 
 
+$(function(){
+
+  if (navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(function(position){
+      geonames_url = "http://ws.geonames.org/findNearbyPostalCodesJSON?formatted=true&lat=" + position.coords.latitude + "&lng="+ position.coords.longitude;
+
+      $.ajax({
+        url: geonames_url,
+        dataType: 'jsonp',
+        success: function(json){
+
+          $("#person_zip").attr("value", json.postalCodes[0].postalCode);
+          $("#person_zip").show();
+        }
+      });
+
+    }, function(error){$("#person_zip").show();});    
+  } else {
+    $("#person_zip").show();
+  }
+
+
+
+})
