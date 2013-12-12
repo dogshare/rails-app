72d0d479ec56bbc260c4dbecf5d3c125c3dba8f0
Charlotte Chang <yellowspiderlove@gmail.com>
2013-12-12 11:33:15 -0500

    basic maps gem added but broken

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
  
   protect_from_forgery with: :exception
+
+  @dogs = Dog.all
+  @hash = Gmaps4rails.build_markers(@dogs) do |dog, marker|
+  marker.lat user.latitude
+  marker.lng user.longitude
+end
 end
